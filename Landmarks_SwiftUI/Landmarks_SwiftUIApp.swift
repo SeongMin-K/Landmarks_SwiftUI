a51ca6a3efa8e073a426f885311387c5959a9070
SeongMin-K <eo1tks3@naver.com>
2021-11-12 19:16:12 +0900

    Initial Commit

---
+++ Landmarks_SwiftUI/Landmarks_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_SwiftUIApp.swift
+//  Landmarks_SwiftUI
+//
+//  Created by SeongMinK on 2021/11/12.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
