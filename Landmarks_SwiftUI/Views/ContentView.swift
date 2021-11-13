127aed5888ba4e632e96867018eb3dd4278a5ce3
SeongMin-K <eo1tks3@naver.com>
2021-11-13 18:49:33 +0900

    Chapter 1. SwiftUI Essentials - Building Lists and Navigation

---
+++ Landmarks_SwiftUI/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks_SwiftUI
+//
+//  Created by SeongMinK on 2021/11/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
